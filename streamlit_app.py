7ebce102942dc527696d07e2e7acc98b9e5d6c36
zendeaashay <148306495+zendeaashay@users.noreply.github.com>
2024-03-25 13:03:56 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import altair as alt
-
+from sklearn.cluster import KMeans
 # Page title
 st.set_page_config(page_title='Happy Cow Case Study Group 7', page_icon='📊')
 st.title('📊 Happy Cow Case Study Group 7')
