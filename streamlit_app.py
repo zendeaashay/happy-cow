68401214da5a500edd39ca51078fd74bbf8bdf07
zendeaashay <148306495+zendeaashay@users.noreply.github.com>
2024-03-27 00:44:38 -0400

    fd de

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,3 +64,47 @@ bar_chart = alt.Chart(filtered_df).mark_bar().encode(
 ).interactive()
 st.altair_chart(bar_chart, use_container_width=True)
 
+from prettymaps import plot
+from matplotlib import pyplot as plt
+
+# Initialize a matplotlib figure
+fig, ax = plt.subplots(figsize = (12, 12), constrained_layout = True)
+
+# Define the location and parameters for the plot
+backup = plot(
+    # Center the map around a location
+    'Hong Kong Island',
+
+    # Define the radius to plot around the central point
+    radius = 1100,
+
+    # Define additional plotting parameters as needed
+    ax = ax,
+
+    # Layers to plot (streets, buildings, etc.)
+    layers = {
+            'perimeter': {},
+            'streets': {
+                'width': {
+                    'motorway': 5,
+                    'primary': 4,
+                    'secondary': 3,
+                    'tertiary': 2,
+                    'residential': 1,
+                    'service': 0.5,
+                    'footway': 0,
+                },
+            },
+            'building': {'tags': {'building': True, 'landuse': ['retail', 'commercial']}},
+            'water': {'tags': {'natural': ['water', 'bay']}},
+            'green': {'tags': {'landuse': 'grass', 'natural': ['island', 'wood']}},
+            'forest': {'tags': {'landuse': 'forest'}},
+            'garden': {'tags': {'leisure': 'garden'}},
+    },
+
+    # Additional parameters can include drawing labels, plotting points, etc.
+)
+
+# Show the plot
+plt.savefig('hong_kong_island.png')
+plt.show()
